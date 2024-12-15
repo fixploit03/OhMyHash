40bd930c3e257b5be44456ec7b392e272cac258f
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2024-12-16 02:25:17 +0700

    Add files via upload

--- src/OhMyHash.sh
+++ src/OhMyHash.sh
@@ -59,7 +59,7 @@ else
 				echo "[+] SHA-512: ${sha512}"
 				echo "[-] Nilai Hash file '${file}' tidak disimpan. Untuk menyimpannya tambahkan opsi -s [FILE] atau --simpan [FILE]."
 			else
-				echo "Error: File '${2}' tidak ditemukan."
+				echo "[-] File '${2}' tidak ditemukan."
 				exit 1
 			fi
 		else
@@ -69,66 +69,74 @@ else
 		if [[ "${1}" == "-f" || "${1}" == "--file" || "${1}" == "-s" || "${1}" == "--simpan" ]]; then
 			if [[ "${1}" == "-f" || "${1}" == "--file" ]]; then
 				if [[ -f "${2}" ]]; then
-					file="${2}"
-					simpan="${4}"
-					md5=$(md5sum "${file}" | awk '{print $1}')
-					sha1=$(sha1sum "${file}" | awk '{print $1}')
-					sha256=$(sha256sum "${file}" | awk '{print $1}')
-					sha512=$(sha512sum "${file}" | awk '{print $1}')
-					copyright
-					echo "[+] Nama file: ${file}"
-					echo "[*] Menghitung nilai Hash file: ${file}"
-					sleep 3
-					echo "[*] Nilai Hash file: ${file}"
-					echo "[+] MD5: ${md5}"
-					echo "[+] SHA-1: ${sha1}"
-					echo "[+] SHA-256: ${sha256}"
-					echo "[+] SHA-512: ${sha512}"
-					echo "[+] Nilai Hash file '${file}' disimpan di: ${simpan}."
-					{
+					if [[ "${3}" == "-s" || "${3}" == "--simpan" ]]; then
+						file="${2}"
+						simpan="${4}"
+						md5=$(md5sum "${file}" | awk '{print $1}')
+						sha1=$(sha1sum "${file}" | awk '{print $1}')
+						sha256=$(sha256sum "${file}" | awk '{print $1}')
+						sha512=$(sha512sum "${file}" | awk '{print $1}')
 						copyright
 						echo "[+] Nama file: ${file}"
+						echo "[*] Menghitung nilai Hash file: ${file}"
+						sleep 3
 						echo "[*] Nilai Hash file: ${file}"
 						echo "[+] MD5: ${md5}"
 						echo "[+] SHA-1: ${sha1}"
 						echo "[+] SHA-256: ${sha256}"
 						echo "[+] SHA-512: ${sha512}"
-					} > "${simpan}"
+						echo "[+] Nilai Hash file '${file}' disimpan di: ${simpan}."
+						{
+							copyright
+							echo "[+] Nama file: ${file}"
+							echo "[*] Nilai Hash file: ${file}"
+							echo "[+] MD5: ${md5}"
+							echo "[+] SHA-1: ${sha1}"
+							echo "[+] SHA-256: ${sha256}"
+							echo "[+] SHA-512: ${sha512}"
+						} > "${simpan}"
+					else
+						menu_bantuan
+					fi
 				else
-					echo "Error: File '${2}' tidak ditemukan."
+					echo "[-] File '${2}' tidak ditemukan."
 					exit 1
 				fi
 
 			elif [[ "${1}" == "-s" || "${1}" == "--simpan" ]]; then
-				if [[ -f "${4}" ]]; then
-					file="${4}"
-					simpan="${2}"
-					md5=$(md5sum "${file}" | awk '{print $1}')
-					sha1=$(sha1sum "${file}" | awk '{print $1}')
-					sha256=$(sha256sum "${file}" | awk '{print $1}')
-					sha512=$(sha512sum "${file}" | awk '{print $1}')
-					copyright
-					echo "[+] Nama file: ${file}"
-					echo "[*] Menghitung nilai Hash file: ${file}"
-					sleep 3
-					echo "[*] Nilai Hash file: ${file}"
-					echo "[+] MD5: ${md5}"
-					echo "[+] SHA-1: ${sha1}"
-					echo "[+] SHA-256: ${sha256}"
-					echo "[+] SHA-512: ${sha512}"
-					echo "[+] Nilai Hash file '${file}' disimpan di: ${simpan}."
-					{
+				if [[ "${3}" == "-f" || "${3}" == "--file" ]]; then
+					if [[ -f "${4}" ]]; then
+						file="${4}"
+						simpan="${2}"
+						md5=$(md5sum "${file}" | awk '{print $1}')
+						sha1=$(sha1sum "${file}" | awk '{print $1}')
+						sha256=$(sha256sum "${file}" | awk '{print $1}')
+						sha512=$(sha512sum "${file}" | awk '{print $1}')
 						copyright
 						echo "[+] Nama file: ${file}"
+						echo "[*] Menghitung nilai Hash file: ${file}"
+						sleep 3
 						echo "[*] Nilai Hash file: ${file}"
 						echo "[+] MD5: ${md5}"
 						echo "[+] SHA-1: ${sha1}"
 						echo "[+] SHA-256: ${sha256}"
 						echo "[+] SHA-512: ${sha512}"
-					} > "${simpan}"
+						echo "[+] Nilai Hash file '${file}' disimpan di: ${simpan}."
+						{
+							copyright
+							echo "[+] Nama file: ${file}"
+							echo "[*] Nilai Hash file: ${file}"
+							echo "[+] MD5: ${md5}"
+							echo "[+] SHA-1: ${sha1}"
+							echo "[+] SHA-256: ${sha256}"
+							echo "[+] SHA-512: ${sha512}"
+						} > "${simpan}"
+					else
+						echo "[-] File '${4}' tidak ditemukan."
+						exit 1
+					fi
 				else
-					echo "Error: File '${2}' tidak ditemukan."
-					exit 1
+					menu_bantuan
 				fi
 			else
 				menu_bantuan
